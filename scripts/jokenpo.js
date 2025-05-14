4516ecd2c21855e15c6a00da1d58ec3b3dd6820e
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2025-05-13 22:44:17 -0300

    first commit

---
+++ scripts/jokenpo.js
@@ -0,0 +1,70 @@
+
+const jsConfetti = new JSConfetti()
+/**
+ *
+ *  ENTRADA
+ *  - A escolha da opção
+ *  PROCESSAMENTO
+ *  - Escolher uma opção para o robo
+ *  - Verificar quem ganhou
+ * SAIDA
+ *  - Ganhador
+ *
+ */
+
+// PEDRA - PAPEL - TESOURA
+function jogarJokenpo(posicaoJogador) {
+  resetImagem();
+  //Criando um vetor
+  //indice de um vetor sempre inicia no zero
+  const opcoes = ["PEDRA", "PAPEL", "TESOURA"];
+
+  //selecionado um valor do vetor
+  // Math - é um lib de operações matematicas
+  // Math.floor - É uma operação de arredondamento
+  // Math.Random - É uma operação de sorteio
+  const sorteio = Math.floor(Math.random() * 3);
+
+  const escolhaRobo = opcoes[sorteio];
+  const escolhaHumano = opcoes[posicaoJogador];
+
+  selecionarImagem(sorteio);
+
+  let elementoResultado = document.getElementById("resultado");
+
+  if (escolhaHumano == "PEDRA" && escolhaRobo == "TESOURA") {
+    elementoResultado.innerHTML = "Humano Ganhou";
+     jsConfetti.addConfetti()
+  } else if (escolhaHumano == "PAPEL" && escolhaRobo == "PEDRA") {
+    elementoResultado.innerHTML = "Humano Ganhou";
+     jsConfetti.addConfetti()
+  } else if (escolhaHumano == "TESOURA" && escolhaRobo == "PAPEL") {
+    elementoResultado.innerHTML = "Humano Ganhou";
+     jsConfetti.addConfetti()
+  } else if (escolhaHumano == escolhaRobo) {
+    elementoResultado.innerHTML = "EMPATOU";
+  } else {
+    elementoResultado.innerHTML = "ROBO Ganhou";
+  }
+
+ 
+}
+
+function selecionarImagem(escolha) {
+  if (escolha == 0) {
+    document.getElementById("roboPedra").classList.remove("naoSelecionada");
+    document.getElementById("roboPedra").classList.add("selecionada");
+  } else if (escolha == 1) {
+    document.getElementById("roboPapel").classList.remove("naoSelecionada");
+    document.getElementById("roboPapel").classList.add("selecionada");
+  } else {
+    document.getElementById("roboTesoura").classList.remove("naoSelecionada");
+    document.getElementById("roboTesoura").classList.add("selecionada");
+  }
+}
+
+function resetImagem() {
+  document.getElementById("roboPedra").classList = "naoSelecionada";
+  document.getElementById("roboPapel").classList = "naoSelecionada";
+  document.getElementById("roboTesoura").classList = "naoSelecionada";
+}
